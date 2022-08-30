9af0f6ea145423cd72b5a61a41fafd353974189d
Nicole <nicole.silfverling@gmail.com>
2022-08-30 11:23:41 +0200

    added buttons

--- src/main/java/com/example/humanrobot/HelloApplication.java
+++ src/main/java/com/example/humanrobot/HelloApplication.java
@@ -3,16 +3,19 @@ package com.example.humanrobot;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hel");
+        Scene scene = new Scene(fxmlLoader.load(), 1080, 1920, Color.BLACK);
+
+        stage.setTitle("Human Robot");
         stage.setScene(scene);
         stage.show();
     }
